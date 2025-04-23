eff67f3ff02a9ae2b78b3d18faffe717aae9219a
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2025-04-22 21:07:42 -0400

    week 2

---
+++ server/src/core/core.module.ts
@@ -0,0 +1,13 @@
+import { ApolloDriver } from "@nestjs/apollo";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { getGraphQLConfig } from "./prisma/config/graphql.config";
+import { PrismaModule } from "../core/prisma/prisma.module";
+
+GraphQLModule.forRootAsync({
+    driver: ApolloDriver,
+    imports: [ConfigModule],
+    useFactory: getGraphQLConfig,
+    inject: [ConfigService]
+  }),
+  PrismaModule
